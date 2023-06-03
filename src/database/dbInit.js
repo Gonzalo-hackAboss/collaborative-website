434c73c4271ad233d4a587edbfd417a735ae7685
Asier <Asierbikandiescuza@gmail.com>
2023-06-03 18:11:41 +0200

    DB actualizado

--- src/database/dbInit.js
+++ src/database/dbInit.js
@@ -2,9 +2,10 @@
 
 require("dotenv").config();
 
-const { getConnection } = require("./mysqlConnection.js");
+const cryptoService = require("../database/services/crypto/service.js");
+const {getConnection} = require("./mysqlConnection.js");
 
-const DATABASE_NAME = "collaborative-website";
+const DATABASE_NAME = process.env.MYSQL_DATABASE;
 
 const initDB = async () =>{
     const pool = getConnection();
@@ -14,10 +15,16 @@ const initDB = async () =>{
     await pool.query(`CREATE DATABASE ${DATABASE_NAME}`);
     await pool.query(`USE ${DATABASE_NAME}`);
     //CREO LA TABLA DE USUARIOS
+    await createDataBaseTables(pool);
+    await insertAdminUsers(pool);
+    await insertModUsers(pool);
+    await pool.end();
+};
+    async function createDataBaseTables(pool){
     await pool.query(`
         CREATE TABLE Users(
             id INT AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(50) NOT NULL,
+            nameMember VARCHAR(50) NOT NULL,
             email VARCHAR(50) NOT NULL UNIQUE,
             password VARCHAR(50) NOT NULL,
             birthday CHAR(8) NOT NULL,
@@ -28,6 +35,7 @@ const initDB = async () =>{
             createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             modifiedAt TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
         )`);
+        //CREO LA TABLA DE POST
     await pool.query(`
         CREATE TABLE Posts(
             id INT AUTO_INCREMENT PRIMARY KEY,
@@ -38,18 +46,21 @@ const initDB = async () =>{
             modifiedAt TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
             FOREIGN KEY (idUser) REFERENCES Users (id)
         )`);
+        //CREO LA TABLA DE TEMAS
     await pool.query(`
         CREATE TABLE Tems(
             id INT AUTO_INCREMENT PRIMARY KEY,
             category VARCHAR(50) NOT NULL,
             description VARCHAR(50) NOT NULL,
         )`)
+        //CREO LA TABLA DE POST IMAGENES
     await pool.query(`
         CREATE TABLE PostImages(
             id INT AUTO_INCREMENT PRIMARY KEY,
             imageURL VARCHAR(300),
             FOREIGN KEY (idPost) REFERENCES Posts (id)
         )`)
+        //CREO LA TABLA DE VALIDACIONES
     await pool.query(`
         CREATE TABLE Validation(
             id INT AUTO_INCREMENT PRIMARY KEY,
@@ -57,6 +68,7 @@ const initDB = async () =>{
             limitTime
             FOREIGN KEY (idUser) REFERENCES Users (id)
         )`)
+        //CREO LA TABLA DE POST DE LOS COMENTARIOS
     await pool.query(`
         CREATE TABLE PostComments(
             id INT AUTO_INCREMENT PRIMARY KEY,
@@ -66,6 +78,7 @@ const initDB = async () =>{
             FOREIGN KEY (idPost) REFERENCES Posts (id)
             FOREIGN KEY (idUser) REFERENCES Users (id)
         )`)
+        //CREO LA TABLA DE VOTOS
     await pool.query(`
         CREATE TABLE Votes(
             id INT AUTO_INCREMENT PRIMARY KEY,
@@ -76,4 +89,123 @@ const initDB = async () =>{
         )`)
 };
 
-initDB(); 
+async function insertAdminUsers(pool) {
+    await pool.execute(
+        `
+        INSERT INTO users(id,name,email,password,birthDate,acceptedTOS,admin) 
+        VALUES(?,?,?,?,?,?,?)  
+        `,
+        [
+            cryptoService.generateUUID(),
+            "Pepe Ruiz",
+            "admin@collaborative-website.com",
+            await cryptoService.hashPassword("password1234"),
+            "1985-06-25",
+            true,
+            true,
+            true,
+        ]
+    );
+}
+
+async function insertModUsers(pool) {
+    await pool.execute(
+        `
+        INSERT INTO users(id,name,email,password,birthDate,acceptedTOS,mod) 
+        VALUES(?,?,?,?,?,?,?)  
+        `,
+        [
+            cryptoService.generateUUID(),
+            "Luis Sanchez",
+            "mod@collaborative-website.com",
+            await cryptoService.hashPassword("password4321"),
+            "1990-03-28",
+            true,
+            true,
+            true,
+        ]
+    );
+}
+
+
+for(const user of user) {
+    await pool.execute(
+        `
+        INSERT INTO users(id,nameMember,email,password,birthDate,acceptedTOS,biography,avatar,country) 
+        VALUES(?,?,?,?,?,?,?,?,?)
+        `,
+        [
+            user.id,
+            user.nameMember,
+            user.email,
+            user.password,
+            user.birthDate,
+            user.acceptedTOS,
+            user.biography,
+            user.avatar,
+            user.coutry,
+        ]
+    );
+}
+
+for(const post of posts) {
+    await pool.execute(
+        `
+        INSERT INTO posts(id,title,description,userId,category)
+        VALUES(?,?,?,?,?)
+        `,
+        [post.id, post.title, post.description, post.userId, post.category]
+    );
+
+for(const tem of tems) {
+    await pool.execute(
+        `
+        INSERT INTO tems(id,category,description)
+        VALUES(?,?,?)
+        `,
+        [tem.id, tem.category, tem.description]
+    );
+}
+
+for(const postImagen of postImagens) {
+    await pool.execute(
+        `
+        INSERT INTO postImagens(id,PostId,image)
+        VALUES(?,?,?)
+        `,
+        [postImagen.id, postImagen.postId, postImagen.image]
+    );
+}
+
+for(const validation of validation) {
+    await pool.execute(
+        `
+        Insert INTO validation(UserId,code,limitTime)
+        Values(?,?,?)
+        `,
+        [validation.userId, validation.code, validation.limitTime]
+    );
+}
+
+for(const postComments of postComments) {
+    await pool.execute(
+        `
+        Insert INTO validation(id,postId,userId,comment)
+        Values(?,?,?,?)
+        `,
+        [postComments.id, postComments.postId, postComments.userId, postComments.comment]
+    );
+}
+
+for(const votes of votes) {
+    await pool.execute(
+        `
+        Insert INTO validation(id,vote,userId,postId)
+        Values(?,?,?,?)
+        `,
+        [votes.id, votes.vote, votes.userId, votes.postId]
+    );
+}
+}
+
+initDB();
