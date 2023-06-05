2d6e09ca8f6b06f0d34e9274347575549c94e5d2
gonza-Rodriguez <gon.rodriguezaquino@gmail.com>
2023-06-05 22:51:20 +0200

    cambios

---
+++ src/user/testLoginUser.js
@@ -0,0 +1,33 @@
+"use strict";
+/**
+ * me está dando un error
+ * [AsyncFunction: loginUser]
+Error: Access denied for user ''@'localhost' (using password: YES)
+Completed running './user/test-loginUser.js'
+revisando .env y conex, db
+ */
+require("dotenv").config();
+
+const loginUser = require("./loginUser.js");
+
+async function testLoginUser() {
+    try {
+        // Caso de prueba: Credenciales válidas
+        const email = "admin@collaborative-website.com";
+        const password = "password1234";
+        const token = await loginUser(email, password);
+        console.log("Token generado:", token);
+
+        // Caso de prueba: Credenciales inválidas (email incorrecto)
+        const invalidEmail = "ana@collaborative-website.com";
+        const invalidPassword = "password3219";
+        const invalidToken = await loginUser(invalidEmail, invalidPassword);
+        console.log("Token generado (inválido):", invalidToken);
+    } catch (error) {
+        console.error("Error:", error.message);
+    }
+}
+
+// Ejecutar la función de prueba
+testLoginUser();
+console.log(loginUser);
---
+++ utils/sendError.js
@@ -0,0 +1,25 @@
+/**
+ * Gestión de Errores inesperados
+ * Recibe el status o el error 500
+ * ** El Error 500 hace referencia a INTERNAL SERVER ERROR (Error)
+ */
+
+module.exports = (res, error) => {
+    console.log(res);
+    res.status(error.status ?? 500).json({
+        success: false,
+        error: {
+            code: error.code ?? "UNEXPECTED_ERROR",
+            msg: error.message ?? "¡Ha ocurrido un error inesperado!",
+        },
+    });
+};
+
+/* 
+AÑADIR POSTERIORMENTE A APP.JS
+app.use ((err, req, res, next) => {
+    sendError(res, err);
+})
+
+IMPORTANTE hacer que SendError llame al archivo y lo requiera
+*/
