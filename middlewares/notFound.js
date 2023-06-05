177a98eeac7afe1458b3109b76b209f17a78f669
gonza-Rodriguez <gon.rodriguezaquino@gmail.com>
2023-06-05 11:14:14 +0200

    errorService

--- middlewares/notFound.js
+++ middlewares/notFound.js
@@ -1,7 +1,9 @@
 "use strict";
+
 const express = require("express");
 const app = express();
 
-module.exports = (req, res, next) => {
+
+//module.exports = (req, res, next) => {
     res.status(404).json({ error: "404, página no encontrada" });
-};
+//};
