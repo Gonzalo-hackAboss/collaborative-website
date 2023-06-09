3a06ac2df7e72c8cc9476d8a07c0b4a69cc1c54d
Anai-1703 <anaingomez.21.22@gmail.com>
2023-06-09 14:00:55 +0200

    soluciona error registerUserTest

--- src/controllers/user/registerUserTest.js
+++ src/controllers/user/registerUserTest.js
@@ -1,19 +1,17 @@
 "use strict";
 const registerUser = require("./registerUser.js");
 const dbService = require("../../services/dbService.js");
-const errorService = require()(async () => {
+const errorService = require("../../services/errorService.js");
+
+(async () => {
     const result = await registerUser({
         email: "anai@test.com",
         password: "nomelose22",
         birthdate: new Date(1967, 2, 17),
         country: "Spain",
         acceptedTOS: true,
-        if (!result) {
-            return errorService.invalidCredentials;
-        }
-
     });
     if (!result) {
-        return errorService.invalidCredentials();
+        return errorService.invalidCredentials, dbService();
     }
 })();
