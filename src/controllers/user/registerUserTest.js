7b9873127b149f72385f2f1cd9f82a437d119a5c
Anai-1703 <anaingomez.21.22@gmail.com>
2023-06-09 17:22:26 +0200

    Guardar cambios antes de cambiar a mi rama ana

--- src/controllers/user/registerUserTest.js
+++ src/controllers/user/registerUserTest.js
@@ -2,18 +2,16 @@
 const registerUser = require("./registerUser.js");
 const dbService = require("../../services/dbService.js");
 
-
 (async () => {
     const result = await registerUser({
         email: "anai@test.com",
         password: "nomelose22",
         birthdate: new Date(1967, 2, 17),
         country: "Spain",
-        acceptedTOS: true
+        acceptedTOS: true,
     });
 
     if (result) {
         return dbService.errorService.invalidCredentials;
     }
 })();
-
