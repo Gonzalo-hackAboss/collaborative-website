8f9b850d355b8df5a12ddce89b50371ec95ba64b
Depechejuan <depechejuan@gmail.com>
2023-06-09 13:02:48 +0200

    seguimos intentando añadir datos a la BBDD

--- src/controllers/user/registerUserTest.js
+++ src/controllers/user/registerUserTest.js
@@ -1,6 +1,8 @@
 "use strict";
 const registerUser = require("./registerUser.js");
 const dbService = require("../../services/dbService.js");
+const errorService = require("../../services/errorService.js");
+
 (async () => {
     const result = await registerUser({
         email: "anai@test.com",
@@ -8,9 +10,8 @@ const dbService = require("../../services/dbService.js");
         birthdate: new Date(1967, 2, 17),
         country: "Spain",
         acceptedTOS: true,
-        if (!result) {
-            return errorService.invalidCredentials;
-        }
-
     });
+    if (!result) {
+        return errorService.invalidCredentials, dbService();
+    }
 })();
