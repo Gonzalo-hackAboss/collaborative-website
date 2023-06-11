4df595834b27dfaee37d464da1aab0ae287fbb89
gonza-Rodriguez <gon.rodriguezaquino@gmail.com>
2023-06-11 11:09:06 +0200

    cambios en cryptoservice, mysqlconnection y validateBody

--- src/database/mysqlConnection.js
+++ src/database/mysqlConnection.js
@@ -5,6 +5,7 @@ const mysql2 = require("mysql2/promise");
 let pool = mysql2.createPool({
     connectionLimit: 10,
     host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MSQYL_DATABASE,
