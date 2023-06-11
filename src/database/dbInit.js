6183edebe024755a87ea3c454302d72dcf9975d1
Depechejuan <depechejuan@gmail.com>
2023-06-11 11:12:16 +0200

    funciona en windows no en linux

--- src/database/dbInit.js
+++ src/database/dbInit.js
@@ -1,30 +1,32 @@
-"use strict";
 require("dotenv").config();
-const { createPool } = require("./mysqlConnection");
+const cryptoService = require("../services/cryptoServices.js");
+const { createPool } = require("./mysqlConnection.js");
+// const { faker } = require("@faker-js/faker");
 
 const DATABASE_NAME = process.env.MYSQL_DATABASE;
 
 const initDB = async () => {
     const pool = createPool();
-    // BORRO LA BASE DE DATOS SI EXISTE
+    console.log("3");
+    //BORRO LA BASE DE DATOS SI EXISTE
     await pool.query(`DROP DATABASE IF EXISTS ${DATABASE_NAME}`);
-
-    // CREO LA BASE DE DATOS
+    console.log("4");
+    //CREO LA BASE DE DATOS
     await pool.query(`CREATE DATABASE ${DATABASE_NAME}`);
     await pool.query(`USE ${DATABASE_NAME}`);
 
-    // CREO LA TABLA DE USUARIOS
-    await createDataBaseTables(pool);
+    //CREO LA TABLA DE USUARIOS
+    await createDatabaseTables(pool);
 
     // await insertAdminUsers(pool);
-    // await generateFakeData(pool);
 
+    // await generateFakeData(pool);
+    console.log("5");
     await pool.end();
 };
 
-async function createDataBaseTables(pool) {
-    await pool.query(`
-    CREATE TABLE IF NOT EXISTS Users(
+async function createDatabaseTables(pool) {
+    await pool.query(`CREATE TABLE IF NOT EXISTS Users(
         id CHAR(36) PRIMARY KEY,
         nameMember VARCHAR(50) NOT NULL,
         email VARCHAR(50) NOT NULL UNIQUE,
@@ -40,7 +42,6 @@ async function createDataBaseTables(pool) {
         modifiedAt TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     );`);
 
-    //CREO LA TABLA DE POST
     await pool.query(`
     CREATE TABLE IF NOT EXISTS Posts(
         id CHAR(36) PRIMARY KEY,
@@ -50,29 +51,25 @@ async function createDataBaseTables(pool) {
         createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         modifiedAt TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
         FOREIGN KEY (idUser) REFERENCES Users (id) ON DELETE CASCADE
-    );`);
-
-    //CREO LA TABLA DE CATEGORIAS
-    await pool.query(`
-    CREATE TABLE IF NOT EXISTS Categories(
-        id CHAR(36) PRIMARY KEY,
-        category ENUM('Salud', 'Politica', 'Deportes', 'Viajes', 'Cocina', 'Internacional, 'Nacional', 'Música') NOT NULL,
-        description VARCHAR(50) NOT NULL
-    );`);
-    // DEFINIR QUE CATEGORIAS USAREMOS!
-
-    // CREAMOS TABLA DE CATEGORIAS Y POSTS PARA BUSCADOR
-    await pool.query(`
-    CREATE TABLE IF NOT EXISTS CategoriesPosts(
-        id CHAR(36) PRIMARY KEY,
-        idPost CHAR(36) NOT NULL,
-        idCategory CHAR(36) NOT NULL,
-        FOREIGN KEY (idPost) REFERENCES Posts (id) ON DELETE CASCADE,
-        FOREIGN KEY (idCategory) REFERENCES Categories (id) ON DELETE CASCADE
     );
     `);
 
-    //CREO LA TABLA DE POST IMAGENES
+    await pool.query(`
+    CREATE TABLE IF NOT EXISTS Categories(
+        id CHAR(36) PRIMARY KEY,
+        category ENUM('Salud', 'Politica', 'Deportes', 'Viajes', 'Cocina', 'Internacional', 'Nacional', 'Música'),
+        description VARCHAR(50) NOT NULL
+    );`);
+
+    await pool.query(`
+    CREATE TABLE IF NOT EXISTS CategoriesPosts(
+	id CHAR(36) PRIMARY KEY,
+	idPost CHAR(36) NOT NULL,
+	idCategory CHAR(36) NOT NULL,
+	FOREIGN KEY (idPost) REFERENCES Posts (id) ON DELETE CASCADE,
+	FOREIGN KEY (idCategory) REFERENCES Categories (id) ON DELETE CASCADE
+);`);
+
     await pool.query(`
     CREATE TABLE IF NOT EXISTS PostImages(
         id CHAR(36) PRIMARY KEY,
@@ -80,9 +77,8 @@ async function createDataBaseTables(pool) {
         imageURL VARCHAR(255),
         createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         FOREIGN KEY (idPost) REFERENCES Posts (id) ON DELETE CASCADE
-    );`);
+        );`);
 
-    //CREO LA TABLA DE VALIDACIONES
     await pool.query(`
     CREATE TABLE IF NOT EXISTS Validation(
         id CHAR(36) PRIMARY KEY,
@@ -90,11 +86,8 @@ async function createDataBaseTables(pool) {
         limitTime VARCHAR(36) NOT NULL,
         idUser CHAR(36) NOT NULL,
         FOREIGN KEY (idUser) REFERENCES Users (id) ON DELETE CASCADE
-    );
-    `);
-    // ¿Debe ser Null el limitTime?
+    );`);
 
-    //CREO LA TABLA DE POST DE LOS COMENTARIOS
     await pool.query(`
     CREATE TABLE IF NOT EXISTS PostComments(
         id CHAR(36) PRIMARY KEY,
@@ -105,41 +98,18 @@ async function createDataBaseTables(pool) {
         modifiedAt TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
         FOREIGN KEY (idPost) REFERENCES Posts (id) ON DELETE CASCADE,
         FOREIGN KEY (idUser) REFERENCES Users (id) ON DELETE CASCADE
-    );
-    `);
+    );`);
 
-    //CREO LA TABLA DE VOTOS
     await pool.query(`
-CREATE TABLE IF NOT EXISTS Votes(
-	id CHAR(36) PRIMARY KEY,
-    idUser CHAR(36) NOT NULL,
-    idPost CHAR(36) NOT NULL,
-	votes BOOL NOT NULL,
-	createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	FOREIGN KEY (idUser) REFERENCES Users (id),
-	FOREIGN KEY (idPost) REFERENCES Posts (id)
-);`);
+    CREATE TABLE IF NOT EXISTS Votes(
+        id CHAR(36) PRIMARY KEY,
+        idUser CHAR(36) NOT NULL,
+        idPost CHAR(36) NOT NULL,
+        votes BOOL NOT NULL,
+        createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (idUser) REFERENCES Users (id),
+        FOREIGN KEY (idPost) REFERENCES Posts (id)
+    );`);
 }
 
 initDB();
-
-// async function generateFakeUsers(pool) {
-//     const [users, posts] = await generateUsersAndPosts();
-//     for (const user of users) {
-//         await pool.execute(
-//             `
-//         INSERT INTO Users(id,nameMember,email,password,birthday,acceptedTOS,validated)
-//         VALUES(?,?,?,?,?,?,?)
-//         `,
-//             [
-//                 user.id,
-//                 user.nameMember,
-//                 user.email,
-//                 user.password,
-//                 user.birthday,
-//                 user.acceptedTOS,
-//                 user.validated,
-//             ]
-//         );
-//     }
-// }
--- src/database/mysqlConnection.js
+++ src/database/mysqlConnection.js
@@ -1,10 +1,13 @@
 const mysql2 = require("mysql2/promise");
 
+//Singleton
 let pool = null;
 function createPool(database) {
+    console.log("1");
     const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD } = process.env;
+    console.log("2");
     return mysql2.createPool({
-        connectionLimit: 15,
+        // connectionLimit: 15,
         host: MYSQL_HOST,
         user: MYSQL_USER,
         database: database,
